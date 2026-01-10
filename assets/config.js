0d18e532788e0ddd6823e2ba0dad37c6f24bbaa5
takahiyo <141232864+takahiyo@users.noreply.github.com>
2026-01-10 16:12:18 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -3,3 +3,7 @@ window.EPUB_READER_CONFIG = window.EPUB_READER_CONFIG || {};
 window.EPUB_READER_CONFIG.googleClientId =
   window.EPUB_READER_CONFIG.googleClientId ||
   '672654349618-h1252pqs19d076dkf3uteme7upau16kp.apps.googleusercontent.com';
+window.APP_CONFIG = {
+  ...
+  GAS_SYNC_ENDPOINT: "https://script.google.com/macros/s/AKfycbzYJcXH8xnJWMYtoPGZOk6xEHJGFgC9cxlkxXfxa3iSXycvsBSxeunLa3BXomIq-pjr/exec"
+};
