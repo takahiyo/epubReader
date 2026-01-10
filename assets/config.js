264810f8f8aba12b9b784c93a085f7b5997832f6
takahiyo <141232864+takahiyo@users.noreply.github.com>
2026-01-11 02:31:47 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -8,5 +8,5 @@ window.EPUB_READER_CONFIG.googleClientId =
 // アプリ共通設定
 window.APP_CONFIG = {
   GAS_SYNC_ENDPOINT:
-    "https://script.google.com/macros/s/AKfycbys-w_F9VnqOVNPSMny4DbPize98bdOCNRPn6NF0gED8qza7ky_P2Z4i3GBimYpl_Dp/exec"
+    "https://script.google.com/macros/s/AKfycbyfXCSewm_mCbV6lHtSaYpHItKjaEWMWJ_uXPqCtJBKPOmhbwqP450jirDqD8AxOTnj/exec"
 };
