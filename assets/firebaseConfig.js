71e9d6ad135964de22150837475651a5e904cfc7
takahiyo <141232864+takahiyo@users.noreply.github.com>
2026-01-13 22:30:34 +0900

    Update Firebase configuration with project details

--- assets/firebaseConfig.js
+++ assets/firebaseConfig.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD2xMk1bbez1Y2crBcgzxUhghU9bFnU1gI",
+  authDomain: "bookreader-1d3a3.firebaseapp.com",
+  projectId: "bookreader-1d3a3",
+  storageBucket: "bookreader-1d3a3.firebasestorage.app",
+  messagingSenderId: "920141070828",
+  appId: "1:920141070828:web:619c658ec726be091c00c9",
+  measurementId: "G-V68746259D"
 };
 
 const app = initializeApp(firebaseConfig);
