64dcce39dd4a0d61663b7551e6a4984f6cc730d8
takahiyo <takahiyo@users.noreply.github.com>
2026-01-22 22:48:45 +0900

    エラー修正2

--- assets/js/core/sync-logic.js
+++ assets/js/core/sync-logic.js
@@ -7,7 +7,7 @@
  */
 
 import { UI_CLASSES, UI_SYMBOLS } from "../../constants.js";
-import { formatRelativeTime } from "../../utils.js";
+import { formatRelativeTime } from "../../i18n.js";
 import { t as t_core, tReplace, getUiStrings } from "../ui/i18n-utils.js";
 import { elements } from "../ui/elements.js";
 import { generateCloudBookId, upsertCloudIndexEntry } from "./file-handler.js";
