c63546741cfb93afb4c7fe8ff8361cc6db35ea82
takahiyo <141232864+takahiyo@users.noreply.github.com>
2026-02-03 13:26:23 +0900

    Update progress.js

--- assets/constants/progress.js
+++ assets/constants/progress.js
@@ -1,3 +1,3 @@
-export const PROGRESS_PRECISION = 0.01;
+export const PROGRESS_PRECISION = 0.1;
 export const PROGRESS_SAVE_THRESHOLD_PERCENT = 0.1;
 export const CLOUD_SYNC_PAGE_THRESHOLD = 3; // クラウド同期を許可するページ差分 (ページ数)
